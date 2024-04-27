f9526ddbd49ddff2f05f4718740dca03d5d87e91
Ivan Belenko <ivan@belenko.com>
2024-04-26 23:37:17 -0700

    Initial Commit

---
+++ Salus/SalusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SalusApp.swift
+//  Salus
+//
+//  Created by Ivan on 4/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SalusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
